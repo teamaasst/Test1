96e5943626a076dbb4ea7d1ddd07e245e27c8182
kishantejy12 <kishantejy12>
2017-08-09 11:59:08 -0500

    First commit

---
+++ Trigger/WebContent/myapp.js
@@ -0,0 +1,29 @@
+var myapp = angular.module('myapp', []);
+
+myapp.controller('MainCtrl', function ($scope) {
+    $scope.showContent = function($fileContent){
+        $scope.content = $fileContent;
+    };
+});
+
+myapp.directive('onReadFile', function ($parse) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs) {
+            var fn = $parse(attrs.onReadFile);
+
+            element.on('change', function(onChangeEvent) {
+                var reader = new FileReader();
+
+                reader.onload = function(onLoadEvent) {
+                    scope.$apply(function() {
+                        fn(scope, {$fileContent:onLoadEvent.target.result});
+                    });
+                };
+
+                reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+            });
+        }
+    };
+});
